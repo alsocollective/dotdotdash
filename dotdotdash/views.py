4dbe51908a9db14018ae6761881c71f19945b141
Symon Oliver <Symon@Symons-MacBook-Pro.local>
2013-07-08 14:13:40 -0400

    styling

--- dotdotdash/views.py
+++ dotdotdash/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from dotdotdash.models import *
 
 def home(request):
-	if(not request.mobile):
+	if(request.mobile):
 		return render_to_response("mobile/index.html",getHome())
 	homeout = getHome()
 	aboutout = getAbout()
